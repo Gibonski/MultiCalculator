2bc91c4fb2f2be1519752f95c6e9bfff4a287351
Olu <a00287691@mycambrian.ca>
2023-10-05 20:51:31 -0400

    My Kotlin multiplatform calculator 1.0

---
+++ shared/Calculator.kt
@@ -0,0 +1,29 @@
+package com.example.multicalculator
+
+class Calculator{
+    fun Add(
+        left: Int,
+        right: Int
+    ): Int {
+        return left + right
+    }
+    fun Subtract(
+        left: Int,
+        right: Int
+    ): Int {
+        return left - right
+    }
+    fun Division(
+        left: Int,
+        right: Int
+    ): Int {
+        return left / right
+    }
+    fun Multiplication(
+        left: Int,
+        right: Int
+    ): Int {
+        return left * right
+    }
+
+}
